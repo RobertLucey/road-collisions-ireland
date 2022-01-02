dc7a18f857f1ccce32ed7b6fcba9f3fa87c4a983
Robert Lucey <robert@glimpse.ie>
2022-01-02 16:47:12 +0000

    Bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='road_collisions_ireland',
-    version='0.0.3',
+    version='0.0.4',
     python_requires='>=3.6',
     description='Road collision data for Ireland',
     author='Robert Lucey',
